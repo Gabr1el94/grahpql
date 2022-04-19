b080a01a4c1be79c3b3a822e700f76e3a20f66d2
Gabr1el94 <bielsystem64@gmail.com>
2022-04-19 20:18:49 -0300

    Type Scalar

--- grahpql-zero/index.js
+++ grahpql-zero/index.js
@@ -1,15 +1,40 @@
 const {gql, ApolloServer } = require("apollo-server");
 
+/**
+ * Scalar Types
+ *  - Int
+ *  - Float
+ *  - String
+ *  - Boolean
+ *  - ID (exclusive)
+*/
+
 const resolvers = {
     Query:{
-        hello(){
-            return "World";
+        age(){
+            return 20;
+        },
+        salary(){
+            return 4330.45;
+        },
+        name(){
+            return "Gabriel";
+        },
+        active(){
+            return true;
+        },
+        id(){
+            return 515445;
         }
     }
 }
 const typeDefs = gql`
     type Query{
-        hello: String
+        age: Int
+        salary: Float
+        name: String
+        active: Boolean
+        id: ID 
     }
 `
 
