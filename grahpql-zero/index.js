96ffbc9b18786944f908eac5380776adcc0eeb18
Gabr1el94 <bielsystem64@gmail.com>
2022-04-21 21:15:21 -0300

    Learning Object Types and Fields

--- grahpql-zero/index.js
+++ grahpql-zero/index.js
@@ -9,38 +9,72 @@ const {gql, ApolloServer } = require("apollo-server");
  *  - ID (exclusive)
 */
 
+/**
+ * Object Types
+ *  => Schema - É uma representação de um objeto
+ *   -> Schema Definition Language(SDL)
+ * -------------------------------------
+ * Arguments
+ */
+
 const resolvers = {
-    Query:{
-        age(){
-            return 20;
+    Query: {
+        users(){
+            return [
+            {
+                id:1,
+                name:'Katia',
+                salary:1234.56,
+                active: false,
+                age: 52
+            },
+            {
+                id:2,
+                name:'Gabriel',
+                salary:3000,
+                active: true,
+                age: 28
+            }
+            ]
         },
-        salary(){
-            return 4330.45;
-        },
-        name(){
-            return "Gabriel";
-        },
-        active(){
-            return true;
-        },
-        id(){
-            return 515445;
-        },
-        technologies(){
-            return ['html','css','js']
+        products(){
+            return [
+            {
+                id:1,
+                name:'Coca-Cola',
+                price:9.90
+            },
+            {
+                id:2,
+                name:'Beer',
+                price:12.90
+            },
+        ]
         }
     }
-}
+};
+
 const typeDefs = gql`
-    type Query{
+
+    type Product {
+        id: ID
+        name: String
+        price: Float
+    }
+
+    type User {
         age: Int
         salary: Float
         name: String
         active: Boolean
         id: ID 
-        technologies: [String!]!
     }
-`
+
+    type Query{
+        users: [User]
+        products: [Product]
+    }
+`;
 
 const server = new ApolloServer({
     typeDefs,
