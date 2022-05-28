80b9b4bfa3ac6086e4ded6522f38f598eebddbaf
Gabr1el94 <bielsystem64@gmail.com>
2022-05-28 15:27:56 -0300

    add constructor

--- api-contacts/src/serivces/UserRegisterService.js
+++ api-contacts/src/serivces/UserRegisterService.js
@@ -1,21 +1,24 @@
 const db = require('../db');
 
 class UserRegisterService {
-    contacts = async () => await db('contacts');
+    constructor(service){
+        this.service = service;
+    }
+    contacts = async () => await this.service('contacts');
     createContact= async (data) => 
-        await (await db("contacts").insert(data).returning('*'))[0];
+        await (await this.service("contacts").insert(data).returning('*'))[0];
     updateContact= async (id,data) =>
-        await (await db("contacts").where({id}).update(data).returning('*'))[0];
+        await (await this.service("contacts").where({id}).update(data).returning('*'))[0];
     deleteContact= async (filter) => {
         if (filter.id) {
-            return await db("contacts").where({id: filter.id}).delete();
+            return await this.service("contacts").where({id: filter.id}).delete();
         }
         if (filter.email) {
-            return await db("contacts").where({email: filter.email}).delete();
+            return await this.service("contacts").where({email: filter.email}).delete();
         }
 
         throw new Error("Error! Please, setter a parameter!!!")
     };
 }
 
-module.exports = new UserRegisterService();
+module.exports = new UserRegisterService(db);
