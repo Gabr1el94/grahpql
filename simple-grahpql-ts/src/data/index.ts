0ecf87ccd42040dbcebd310d86c2631be558f680
Gabr1el94 <bielsystem64@gmail.com>
2022-09-04 14:33:25 -0300

    simple graphql in typescript

---
+++ simple-grahpql-ts/src/data/index.ts
@@ -0,0 +1,5 @@
+export default [
+    { id: 1, name: "Gabriel Soares", email: "gsstest@gmail.com"},
+    { id: 1, name: "Katia Soares", email: "ksstest@gmail.com"},
+    { id: 1, name: "Arthur Soares", email: "arthurtest@gmail.com"},
+]
