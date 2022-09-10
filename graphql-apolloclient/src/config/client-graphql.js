60c22c6e393ea0c9741e37561a5a1308e75f6fe9
Gabr1el94 <bielsystem64@gmail.com>
2022-09-10 16:28:59 -0300

    add public

--- graphql-apolloclient/src/config/client-graphql.js
+++ graphql-apolloclient/src/config/client-graphql.js
@@ -1,6 +1,6 @@
-import { ApolloServer, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-    uri: 'http://localhost:400',
+    uri: 'http://localhost:4000',
     cache: new InMemoryCache(),
 });
